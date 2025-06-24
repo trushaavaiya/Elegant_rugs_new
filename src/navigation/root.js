d68e14772e6b1ffec8cc0c04e763dae78e6a7b5a
Trusha Avaiya <trushaavaiya@gmail.com>
2025-06-24 19:22:13 +0530

    import statemnt for screen 3 to 7

--- src/navigation/root.js
+++ src/navigation/root.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import SplashScreen from '../screens/Splash';
-import Splash2 from '../screens/Splash2';
+import SplashScreen from '../screens/Splash';                         //1
+import Splash2 from '../screens/Splash2';                             //2
+import LoginScreen from '../screens/LoginScreen';                     //3
+import ForgotPassword from '../screens/ForgotPassword';               //4
+import NewPasswordScreen from '../screens/NewPasswordScreen';         //5
+import ResetPasswordScreen from '../screens/ResetPasswordScreen';     //6
+import SignupPage from '../screens/SignupPage';                       //7
 
 const Stack = createStackNavigator();
 
@@ -16,6 +21,12 @@ const Root = () => {
       >
         <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="Splash2" component={Splash2} />
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+        <Stack.Screen name="NewPasswordScreen" component={NewPasswordScreen} />
+        <Stack.Screen name="ResetPasswordScreen" component={ResetPasswordScreen} />
+        <Stack.Screen name="SignupPage" component={SignupPage} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
