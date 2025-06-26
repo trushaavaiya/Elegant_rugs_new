36b3668a6969e608d625f6de9df068f03814b066
Trusha Avaiya <trushaavaiya@gmail.com>
2025-06-26 10:00:47 +0530

    Rename  the 3 file name

--- src/navigation/root.js
+++ src/navigation/root.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import SplashScreen from '../screens/Splash';                         //1
-import Splash2 from '../screens/Splash2';                             //2
+import SplashScreen from '../screens/SplashScreen';                         //1
+import Splash2 from '../screens/Splash2Screen';                             //2
 import LoginScreen from '../screens/LoginScreen';                     //3
 import ForgotPassword from '../screens/ForgotPassword';               //4
 import NewPasswordScreen from '../screens/NewPasswordScreen';         //5
 import ResetPasswordScreen from '../screens/ResetPasswordScreen';     //6
-import SignupPage from '../screens/SignupPage';                       //7
-import IntroScreen1 from '../screens/IntroScreen1';
-import IntroScreen2 from '../screens/IntroScreen2';
-import IntroScreen3 from '../screens/IntroScreen3';
+import SignupScreen from '../screens/SignupScreen';                       //7
+import IntroScreen1 from '../screens/IntroScreen1';                   //8
+import IntroScreen2 from '../screens/IntroScreen2';                   //9
+import IntroScreen3 from '../screens/IntroScreen3';                   //10
 
 
 
@@ -30,7 +30,7 @@ const Root = () => {
         <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
         <Stack.Screen name="NewPasswordScreen" component={NewPasswordScreen} />
         <Stack.Screen name="ResetPasswordScreen" component={ResetPasswordScreen} />
-        <Stack.Screen name="SignupPage" component={SignupPage} />
+        <Stack.Screen name="SignupScreen" component={SignupScreen} />
         <Stack.Screen name="IntroScreen1" component={IntroScreen1} />
         <Stack.Screen name="IntroScreen2" component={IntroScreen2} />
         <Stack.Screen name="IntroScreen3" component={IntroScreen3} />
