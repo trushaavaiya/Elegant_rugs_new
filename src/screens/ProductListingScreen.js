5f51827397914911fe4ff6e0029a4f2479d8a8a1
Trusha Avaiya <trushaavaiya@gmail.com>
2025-07-02 11:04:32 +0530

    ProductListingScreen :- import basic statement

---
+++ src/screens/ProductListingScreen.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  FlatList,
+  TouchableOpacity,
+  SafeAreaView,
+  Dimensions,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import Colors from '../constants/Color';
+import Fonts from '../constants/Font';
+
+
+
+const ProductListingScreen = ({ navigation }) => {
+  
+  
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default ProductListingScreen;
