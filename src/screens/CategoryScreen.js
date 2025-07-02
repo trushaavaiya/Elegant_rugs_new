d085e0dda2403070a0149a9261d0ff8da62d717b
Trusha Avaiya <trushaavaiya@gmail.com>
2025-07-02 10:26:26 +0530

    CategoryScreen:- basic statement

---
+++ src/screens/CategoryScreen.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+  Image,
+  SafeAreaView,
+  Dimensions,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import Colors from '../constants/Color';
+import Fonts from '../constants/Font';
+
+
+
+
+const CategoryScreen = ({ navigation }) => {
+
+  
+  return (
+    <SafeAreaView style={styles.container}>
+     
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default CategoryScreen;
