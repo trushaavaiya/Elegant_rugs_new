f7af35d3616785d8a4a689b1cec5c7e78837d8d0
Trusha Avaiya <trushaavaiya@gmail.com>
2025-07-08 21:14:20 +0530

    ALLReviews :- added basic statement

---
+++ src/screens/AllReviews.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  Image,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+  SafeAreaView,
+  StatusBar,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import Colors from '../constants/Color';
+import Fonts from '../constants/Font';
+
+const { width } = Dimensions.get('window');
+
+
+
+const AllReviews = () => {
+  return (
+    <View></View>
+);
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default AllReviews;
