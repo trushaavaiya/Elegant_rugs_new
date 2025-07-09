1762910c4d8c3c803710012d8e8b6402fceaaf9a
Trusha Avaiya <trushaavaiya@gmail.com>
2025-07-09 10:27:32 +0530

    Root :- added import  statement

--- src/navigation/root.js
+++ src/navigation/root.js
@@ -2,22 +2,23 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-// import SplashScreen from '../screens/SplashScreen';                   //1
-// import Splash2Screen from '../screens/Splash2Screen';                 //2
-// import LoginScreen from '../screens/LoginScreen';                     //3
-// import ForgotPasswordScreen from '../screens/ForgotPasswordScreen';   //4
-// import NewPasswordScreen from '../screens/NewPasswordScreen';         //5
-// import ResetPasswordScreen from '../screens/ResetPasswordScreen';     //6
-// import SignupScreen from '../screens/SignupScreen';                   //7
-// import IntroScreen1 from '../screens/IntroScreen1';                   //8
-// import IntroScreen2 from '../screens/IntroScreen2';                   //9
-// import IntroScreen3 from '../screens/IntroScreen3';                   //10
-// import HomeScreen from '../screens/HomeScreen';                       //11
-// import CategoryScreen from '../screens/CategoryScreen';               //12
-// import ProductListingScreen from '../screens/ProductListingScreen';   //13
-// import FilterModal from '../screens/FilterModal';                     //14
-// import ProductDetails from '../screens/ProductDetails';               //15
+import SplashScreen from '../screens/SplashScreen';                   //1
+import Splash2Screen from '../screens/Splash2Screen';                 //2
+import LoginScreen from '../screens/LoginScreen';                     //3
+import ForgotPasswordScreen from '../screens/ForgotPasswordScreen';   //4
+import NewPasswordScreen from '../screens/NewPasswordScreen';         //5
+import ResetPasswordScreen from '../screens/ResetPasswordScreen';     //6
+import SignupScreen from '../screens/SignupScreen';                   //7
+import IntroScreen1 from '../screens/IntroScreen1';                   //8
+import IntroScreen2 from '../screens/IntroScreen2';                   //9
+import IntroScreen3 from '../screens/IntroScreen3';                   //10
+import HomeScreen from '../screens/HomeScreen';                       //11
+import CategoryScreen from '../screens/CategoryScreen';               //12
+import ProductListingScreen from '../screens/ProductListingScreen';   //13
+import FilterModal from '../screens/FilterModal';                     //14
+import ProductDetails from '../screens/ProductDetails';               //15
 import AllReviews from '../screens/AllReviews';                       //16
+import BulkBookingModal from '../screens/BulkBookingModal';           //17
 
 
 const Stack = createStackNavigator();
@@ -29,7 +30,7 @@ const Root = () => {
         initialRouteName="Splash"
         screenOptions={{ headerShown: false }}  
       >
-        {/* <Stack.Screen name="Splash" component={SplashScreen} />
+        <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="Splash2Screen" component={Splash2Screen} />
         <Stack.Screen name="LoginScreen" component={LoginScreen} />
         <Stack.Screen name="ForgotPasswordScreen" component={ForgotPasswordScreen} />
@@ -43,8 +44,9 @@ const Root = () => {
         <Stack.Screen name="CategoryScreen" component={CategoryScreen} />
         <Stack.Screen name="ProductListingScreen" component={ProductListingScreen} />
         <Stack.Screen name="FilterModal" component={FilterModal} />
-        <Stack.Screen name="ProductDetails" component={ProductDetails} /> */}
+        <Stack.Screen name="ProductDetails" component={ProductDetails} />
         <Stack.Screen name="AllReviews" component={AllReviews} />
+        <Stack.Screen name="BulkBookingModal" component={BulkBookingModal} />
 
 
       </Stack.Navigator>
