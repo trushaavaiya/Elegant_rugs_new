ec23dfa707f1db4963b5a77a04b887bc9e2400a1
Trusha Avaiya <trushaavaiya@gmail.com>
2025-07-09 10:22:16 +0530

    BulkBookingModal : - added basic statement

---
+++ src/screens/BulkBookingModal.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  Modal,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+  ImageBackground,
+} from 'react-native';
+import Colors from '../constants/Color';
+import Fonts from '../constants/Font';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { useNavigation } from '@react-navigation/native';
+
+const { width, height } = Dimensions.get('window');
+
+
+const BulkBookingModal = ({}) => {
+ 
+
+  return (
+    <Modal>
+      
+    </Modal>
+    
+);
+};
+
+export default BulkBookingModal;
+
+const styles = StyleSheet.create({
+  
+});
